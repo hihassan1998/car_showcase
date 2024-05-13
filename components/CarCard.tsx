cc03bb028ba0a2a32d3cded4fa29568d24247421
hihassan1998 <hassanihussain1998@gmail.com>
2024-05-13 18:46:35 +0200

    created carCard component for displaying appi results and example img set for all cards

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,83 @@
-import React from 'react'
+"use client";
 
-const CarCard = () => {
+import { CarProps } from '@/types';
+import Image from 'next/image';
+
+import { useState } from 'react'
+import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps {
+  car: CarProps
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const carRent = calculateCarRent(city_mpg, year);
+
+
   return (
-    <div>CarCard</div>
+    <div className="car-card group">
+      <div className='car-card__content-title'>
+        <h2>{make} {model}
+        </h2>
+      </div>
+      <p className='felx mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>
+          sek
+        </span>
+        {carRent}
+        <span className='self-end text-[14px] font-medium'>
+          /dag
+        </span>
+      </p>
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image src="/hero.png" alt='car model' fill priority className='object-contain'
+        /> {/*width={500}  height={500} cant be used with fill property*/}
+      </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src="/steering-wheel.svg" width={20} height={20} alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? 'Automatisk' : 'Manuell'}
+            </p>
+          </div>
+
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src="/tire.svg" width={20} height={20} alt='tire'
+            />
+            <p className='text-[14px]'>
+              {drive.toUpperCase()}
+            </p>
+          </div>
+
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src="/gas.svg" width={20} height={20} alt='gas'
+            />
+            <p className='text-[14px]'>
+              {city_mpg} MPG
+            </p>
+          </div>
+        </div>
+        <div className='car-card__btn-container'>
+          <CustomButton
+            title='Visa Mer'
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon='/right-arrow.svg'
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
