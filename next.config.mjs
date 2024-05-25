eba2682625f3d553cb8b502749dbc92c1eae633f
hihassan1998 <hassanihussain1998@gmail.com>
2024-05-25 19:16:06 +0200

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.imagin.studio',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.imagin.studio',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['i.ibb.co'],
   },
 };
+
 
 export default nextConfig;
